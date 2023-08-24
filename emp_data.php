b783b0e129d5d9f2ccb193952d06f461ccb40bb3
saptarshi92 <saptarshi34.sc@gmail.com>
2023-08-24 18:12:57 +0530

    employee-data

---
+++ emp_data.php
@@ -0,0 +1,45 @@
+<?php
+$emp_details = file_get_contents('salaries.json');
+$emp_details = json_decode($emp_details, true);
+
+
+$max_salary = [];
+$salary = [];
+$max_sal = 0;
+$max_sal_emp_details = [];
+$min_sal_emp_details = [];
+//print_r('Number of Employees::'.count($emp_details).PHP_EOL);
+foreach ($emp_details as $emp_detail) {
+    foreach ($emp_detail as $key => $value) {
+        if ($key == 'Name') {
+            $name = $value;
+
+        } elseif ($key == 'Age') {
+            $age = $value;
+        } elseif ($key == 'Salary') {
+            $salary[] = $value;
+            if ($value > $max_sal) {
+                $max_sal = $value;
+                $max_sal_emp_details = array(
+                    'name' => $name,
+                    'Salary' => $max_sal,
+                    'Age' => $age
+
+                );
+            }
+            elseif ($value < $max_sal) {
+                $min_sal = $value;
+                $min_sal_emp_details = array(
+                    'name' => $name,
+                    'Salary' => $min_sal,
+                    'Age' => $age
+
+                );
+            }
+
+        }
+    }
+}
+print_r($min_sal_emp_details);
+print_r($max_sal_emp_details);
+?>
